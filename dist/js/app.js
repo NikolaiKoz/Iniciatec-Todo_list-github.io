c654f59d46aaa73b6e9a349b3513bc5c2fda09e6
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-12 09:03:41 -0300

    ci: :package: The scripts are ready to start working.

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,4 +2,10 @@ console.log('Hello World');
 console.log('I am a TypeScript file');
 console.log('I am a TypeScript file');
 console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
 //# sourceMappingURL=app.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,3 +3,12 @@ console.log('Hello World');
 console.log('I am a TypeScript file');
 console.log('I am a TypeScript file');
 console.log('I am a TypeScript file');
+
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+
+
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
+console.log('I am a TypeScript file');
