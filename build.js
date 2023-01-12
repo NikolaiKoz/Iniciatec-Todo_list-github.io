954e9fd96ba424d2170534d5763c2714cb0b5a3d
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-12 12:27:01 -0300

    feat: :art: Basic structure of html DONE.

--- build.js
+++
@@ -1,9 +0,0 @@
-const pug = require('pug');
-const fs = require('fs');
-
-const inputFile = './src/index.pug';
-const outputFile = './dist/app.html';
-const html = pug.renderFile(inputFile);
-
-fs.writeFileSync(outputFile, html);
-console.log(`Compilado ${inputFile} a ${outputFile}`);
